16864de7616727fa95e1aedf024d89c7880fd880
无缘缘 <3012935320@qq.com>
2025-12-28 18:14:53 +0800

    fix: actually update base path for Vercel support

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/smart-learn-pro/', // Set base path for GitHub Pages
+    base: process.env.GITHUB_ACTIONS === 'true' ? '/smart-learn-pro/' : '/', // Use subpath for GH Pages, root for Vercel/Others
     optimizeDeps: {
         exclude: ['react-resizable-panels']
     }
