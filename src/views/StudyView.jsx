cce577185c5e56886c57d87c625efbf05d3739e2
无缘缘 <3012935320@qq.com>
2025-12-28 23:42:17 +0800

    fix: missing useEffect import in StudyView

--- src/views/StudyView.jsx
+++ src/views/StudyView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Brain, NotebookPen, Layers, Sparkles, X, Loader, FileText } from 'lucide-react';
 import { useApp } from '../context/AppContext';
 import WordCard from '../components/WordCard';
